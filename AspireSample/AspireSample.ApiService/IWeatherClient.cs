b833c5112e43d45bccd12b4b81b8086dd06bdfcd
Anton Skuratovich <a.skuratovich@gmail.com>
2024-01-30 23:40:58 +0100

    step3: add external api

---
+++ AspireSample/AspireSample.ApiService/IWeatherClient.cs
@@ -0,0 +1,56 @@
+using System.Web;
+
+namespace AspireSample.ApiService;
+
+public interface IWeatherClient
+{
+    Task<WeatherForecast[]> GetWeatherForecastAsync(string city, CancellationToken cancellationToken = default);
+}
+
+internal sealed class WeatherClient(HttpClient httpClient): IWeatherClient
+{
+    public async Task<WeatherForecast[]> GetWeatherForecastAsync(string city, CancellationToken cancellationToken = default)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(city);
+
+        string escapedCityString = Uri.EscapeDataString(city);
+
+        var requestUri = new Uri($"v4/weather/forecast?location={escapedCityString}&timesteps=1d&units=metric", UriKind.Relative);
+
+        var response = await httpClient.GetFromJsonAsync<TomorrowWeatherForecast>(
+            requestUri,
+            cancellationToken);
+
+        return response is not null
+            ? response.Timelines.Daily
+                .Select(x => new WeatherForecast(DateOnly.FromDateTime(x.Time), (int)x.Values.TemperatureAvg, "Mild"))
+                .ToArray()
+            : [];
+    }
+}
+
+internal class ApiKeyHandler(string apiKey) : DelegatingHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        // Add the API key as a query parameter to all requests
+        var uriBuilder = new UriBuilder(request.RequestUri!);
+        var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+        query["apikey"] = apiKey;
+        uriBuilder.Query = query.ToString();
+
+        request.RequestUri = uriBuilder.Uri;
+
+        return base.SendAsync(request, cancellationToken);
+    }
+}
+
+internal sealed record TomorrowWeatherForecast(Timelines Timelines);
+
+internal sealed record Timelines(Timeline[] Daily);
+
+internal sealed record Timeline(DateTime Time, Values Values);
+
+internal sealed record Values(float TemperatureAvg);
--- AspireSample/AspireSample.ApiService/Program.cs
+++ AspireSample/AspireSample.ApiService/Program.cs
@@ -1,3 +1,5 @@
+using AspireSample.ApiService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire components.
@@ -6,34 +8,32 @@ builder.AddServiceDefaults();
 // Add services to the container.
 builder.Services.AddProblemDetails();
 
+builder.Services.AddHttpClient<IWeatherClient, WeatherClient>(client =>
+{
+    var baseAddress = builder.Configuration["WeatherClient:BaseAddress"];
+    client.BaseAddress = new Uri(baseAddress);
+})
+.AddHttpMessageHandler(() =>
+{
+    var apiKey = builder.Configuration["WeatherClient:ApiKey"];
+    return new ApiKeyHandler(apiKey);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
-var summaries = new[]
+app.MapGet("/weatherforecast", async (IWeatherClient weatherClient, CancellationToken cancellationToken) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
+    var forecast = await weatherClient.GetWeatherForecastAsync("new york", cancellationToken);
     return forecast;
 });
 
 app.MapDefaultEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
